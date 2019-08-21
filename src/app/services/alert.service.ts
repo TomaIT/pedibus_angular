8b6c650d805a26c117e3e320d7d18875614894c4
Giovanni Tomasino <giovanni.tomasino.it@gmail.com>
2019-08-21 16:50:06 +0200

    add alert service

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {NavigationStart, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
-// TODO
-  constructor() { }
+  private subject = new Subject<any>();
+  private keepAfterNavigationChange = false;
+
+  constructor(private router: Router) {
+    // clear alert message on route change
+    router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (this.keepAfterNavigationChange) {
+          // only keep for a single location change
+          this.keepAfterNavigationChange = false;
+        } else {
+          // clear alert
+          this.subject.next();
+        }
+      }
+    });
+  }
+
+  clearAlert() {
+    this.subject.next();
+  }
+
+  success(message: string, keepAfterNavigationChange = false) {
+    this.keepAfterNavigationChange = keepAfterNavigationChange;
+    this.subject.next({type: 'success', text: message});
+    window.scroll(0, 0);
+  }
+
+  error(message: string, keepAfterNavigationChange = false) {
+    this.keepAfterNavigationChange = keepAfterNavigationChange;
+    this.subject.next({type: 'error', text: message});
+    window.scroll(0, 0);
+  }
+
+  getMessage(): Observable<any> {
+    return this.subject.asObservable();
+  }
 }
