c2aa40efeadbff475c78f5cf5ccc79c2f6b74917
TomaIT <giovanni.tomasino.it@gmail.com>
2019-08-25 10:46:29 +0200

    aggiornato availability state

--- src/app/models/availability.ts
+++ src/app/models/availability.ts
@@ -3,7 +3,13 @@ import {BusRide} from './busride';
 export enum AvailabilityState {
   available = 'Available', // Escort è disponibile a coprire quella corsa
   checked = 'Checked', // Admin ha confermato, e attende che Escort riconfermi la sua disponibilità
+  readChecked = 'ReadChecked', // Escort conferma la diponibilità.
+  confirmed = 'Confirmed'  // Admin chiude la disponibilità, essa non cambierà più
+ /* OLD VERSION
+  available = 'Available', // Escort è disponibile a coprire quella corsa
+  checked = 'Checked', // Admin ha confermato, e attende che Escort riconfermi la sua disponibilità
   confirmed = 'Confirmed' // Escort conferma infine la diponibilità, essa non cambierà più.
+ */
 }
 
 export class Availability {
