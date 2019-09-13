46a794da74100a28ec829893378700ffee72d5ca
TomaIT <giovanni.tomasino.it@gmail.com>
2019-09-13 16:22:35 +0200

    aggiunto messaggio errore javascript bloccato

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1/rest',
+  apiUrl: 'http://pedibus.ns0.it/rest',
   intervalTimePolling: 1000, // milliseconds
   intervalAvailCheck: 5000,
   startingInterval: 30 // minutes
